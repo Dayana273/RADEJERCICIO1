f6d171c7f6bf92d6c255b535d09899e1f242b66c
Dayana <Dayana@DESKTOP-BMET23Q>
2023-06-17 14:07:53 -0600

    Agregar archivos de proyecto.

---
+++ RADEJERCICIO1/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RADEJERCICIO1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btEnviar_Clicked_1(object sender, EventArgs e)
+        {
+
+
+            {
+
+
+                var person = new Personas
+                {
+                    nombre = txtnombre.Text,
+                    Apellido = txtApellido.Text,
+                    correo = txtcorreo.Text,
+                    Edad = txtedad.Text,    
+                    direccion = txtdireccion.Text,
+
+                };
+                //esto es para llamar una segunda pagina y pasar un parametro a una segunda pantalla//
+                var seconpage = new PageResultado();
+                seconpage.BindingContext = person; //paso de parametros a la pantalla
+                Navigation.PushAsync(seconpage);
+            }
+
+
+        }
+    }
+}
+
---
+++ RADEJERCICIO1/Personas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RADEJERCICIO1
+{
+    public class Personas : ContentPage
+    {
+
+
+
+        public int Id { get; set; }
+
+        
+        public string nombre { get; set; }
+
+        
+        public string Apellido { get; set; }
+
+        public string Edad { get; set; }
+
+        public string correo { get; set; }
+
+        
+        public string direccion { get; set; }
+
+    }
+}
